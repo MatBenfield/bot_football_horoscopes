0e26ffeabedde0c5a76a33b7d6e03d4ddac3c639
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-09-21 13:01:39 +0100

    Create cron.class.php

---
+++ cron.class.php
@@ -0,0 +1,23 @@
+<?php
+	/*
+	Plugin Name: TCR Cron Class
+	Description: Adds a basic cron methods
+	Version: 10.0.0
+	Plugin URI: http://thecellarroom.uk
+	Author: ChelseaStats
+	Author URI: http://thecellarroom.uk
+	Copyright (c) 2015 The Cellar Room Limited
+	*/
+	class CronDB {
+	
+		public function Tweet($message) {
+				$connection = new TwitterOAuth(**,**,**,**); // redacted
+				$connection->get('account/verify_credentials');
+				$connection->post('statuses/update',array('status' => $message));
+			}
+		public function getValueFromKey($array) {
+			$rand_key = array_rand($array, 1);
+			$value = $array[$rand_key];
+			return $value;
+		}
+	}
